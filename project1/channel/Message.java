0020a59e3c5f22bf74bd0b93f8387c8834e22e2c
cyrilico <jpdamas15@gmail.com>
2018-03-26 21:45:34 +0100

    Add similar methods to Message but to work with byte[]

--- project1/channel/Message.java
+++ project1/channel/Message.java
@@ -7,7 +7,7 @@ enum MessageType {
     }
 
 public class Message {
-    public static String CRLF = "\\r\\n";
+    public static String CRLF = "\r\n";
 
     public MessageType type;
     public String version;
@@ -18,42 +18,114 @@ public class Message {
     public Integer repDegree = null;
     public byte[] body = null;
 
+    //Processes a message's header line
+    public void processHeader(String str) {
+      String[] header = str.split("\\s+");
+      switch(header[0]){
+          case "PUTCHUNK":
+              this.type = MessageType.PUTCHUNK;
+              this.repDegree = Integer.parseInt(header[5]);
+              break;
+          case "STORED":
+              this.type = MessageType.STORED;
+              break;
+          case "GETCHUNK":
+              this.type = MessageType.GETCHUNK;
+              break;
+          case "CHUNK":
+              this.type = MessageType.CHUNK;
+              break;
+          case "DELETE":
+              this.type = MessageType.DELETE;
+              break;
+          case "REMOVED":
+              this.type = MessageType.REMOVED;
+              break;
+          default:
+              break;
+      }
+
+      this.version = header[1];
+      this.peerID = Integer.parseInt(header[2]);
+      this.fileID = header[3];
+      if(header.length > 4)
+        this.chunkNr = Integer.parseInt(header[4]);
+    }
+
+    //Processes a message given as a String
     public Message(String messageStr) {
         //Should split into two elements: 0 = header, 1 = body (if present)
         String[] messageComponents = messageStr.split("\\R\\R", 2);
         if(messageComponents.length > 1)
           this.body = messageComponents[1].getBytes();
 
-        String[] header = messageComponents[0].split("\\s+");
-        switch(header[0]){
-            case "PUTCHUNK":
-                this.type = MessageType.PUTCHUNK;
-                this.repDegree = Integer.parseInt(header[5]);
-                break;
-            case "STORED":
-                this.type = MessageType.STORED;
-                break;
-            case "GETCHUNK":
-                this.type = MessageType.GETCHUNK;
-                break;
-            case "CHUNK":
-                this.type = MessageType.CHUNK;
-                break;
-            case "DELETE":
-                this.type = MessageType.DELETE;
-                break;
-            case "REMOVED":
-                this.type = MessageType.REMOVED;
-                break;
-            default:
-                break;
-        }
+        processHeader(messageComponents[0]);
+    }
 
-        this.version = header[1];
-        this.peerID = Integer.parseInt(header[2]);
-        this.fileID = header[3];
-        if(header.length > 4)
-          this.chunkNr = Integer.parseInt(header[4]);
+    //Processes a message given as a byte[] (directly from a DatagramPacket)
+    public Message(byte[] message) {
+      int headerLength = 0;
+      for (int i = 0; i < message.length; ++i) {
+            System.out.println("Char: "+(char)message[i]);
+            if((char)message[i] == '\r') { //TODO: Parse better
+              headerLength = i+1;
+              break;
+            }
+      }
+
+      String header = new String(message, 0, headerLength);
+      processHeader(header);
+      this.body = new byte[message.length - headerLength];
+      System.arraycopy(message, headerLength, this.body, 0, message.length-headerLength);
+    }
+
+    public byte[] buildMessagePacket() {
+      StringBuilder result = new StringBuilder();
+      switch(this.type){
+          case PUTCHUNK:
+              result.append("PUTCHUNK ");
+              break;
+          case STORED:
+              result.append("STORED ");
+              break;
+          case GETCHUNK:
+              result.append("GETCHUNK ");
+              break;
+          case CHUNK:
+              result.append("CHUNK ");
+              break;
+          case DELETE:
+              result.append("DELETE ");
+              break;
+          case REMOVED:
+              result.append("REMOVED ");
+              break;
+          default:
+              break;
+      }
+
+      result.append(this.version);
+      result.append(" ");
+      result.append(this.peerID);
+      result.append(" ");
+      result.append(this.fileID);
+      result.append(" ");
+      if(this.chunkNr != null) {
+        result.append(this.chunkNr);
+        result.append(" ");
+      }
+      if(this.repDegree != null) {
+        result.append(this.repDegree);
+        result.append(" ");
+      }
+      result.append(Message.CRLF+Message.CRLF);
+
+      String header = result.toString();
+      byte[] packet = new byte[header.length+this.body.length];
+      System.arraycopy(header, 0, packet, 0, header.length);
+      System.arraycopy(this.body, 0, packet, header.length, this.body.length);
+
+      return packet;
     }
 
     @Override
@@ -98,7 +170,7 @@ public class Message {
       }
       result.append(Message.CRLF+Message.CRLF);
       if(this.body != null)
-      result.append(this.body);
+      result.append(new String(this.body));
 
       return result.toString();
     }
